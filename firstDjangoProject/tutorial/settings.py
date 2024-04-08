9f9d97882aff27cacba2895abbe60a01c1a92db9
giggio <enzogiglio.office@gmail.com>
2024-04-08 23:42:49 +0200

    test comment added.

--- firstDjangoProject/tutorial/settings.py
+++ firstDjangoProject/tutorial/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Adding this line to test whether the integrated git versioning control works or not
